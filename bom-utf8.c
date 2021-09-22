e575d588422c54df85466a450c5dc564cf51b70d
Subin <39542978+subeans@users.noreply.github.com>
2021-09-22 22:56:16 +0900

    bom code and uni code

---
+++ bom-utf8.c
@@ -0,0 +1,13 @@
+// 유니코드 자음, 모음 , 초/중/종성 코드 -- UTF-8 출력 
+//	C> a.exe > output.txt
+//		--> output.txt�� �޸��忡�� Ȯ��
+// ���δ��б� ����Ʈ�����к� ���½�
+#include <stdio.h>
+
+int main() {
+	printf("%c%c%c", 0xEF, 0xBB, 0xBF);	// UTF-8 BOM code
+	printf("A. %c%c%c\n", 0xEA, 0xB0, 0x80);	// '가'
+	printf("B. %c%c%c\n", 0xED, 0x9E, 0xA3);	// '힣'
+
+	return 0;
+}
